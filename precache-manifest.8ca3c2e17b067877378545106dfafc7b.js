361382e2fda52aeea7e7b16f2c40d3a84503957a
unknown <anishmohamned62@gmail.com>
2023-11-29 21:04:49 +0530

    Updates

---
+++ precache-manifest.8ca3c2e17b067877378545106dfafc7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91f85f78fea080b9a47e408e025c182",
+    "url": "/Calcculator/index.html"
+  },
+  {
+    "revision": "0a958025aa9f26bb3123",
+    "url": "/Calcculator/static/css/main.efb92317.chunk.css"
+  },
+  {
+    "revision": "55d9828ca1827bb8cae8",
+    "url": "/Calcculator/static/js/2.a5d8ed2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calcculator/static/js/2.a5d8ed2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a958025aa9f26bb3123",
+    "url": "/Calcculator/static/js/main.2437682a.chunk.js"
+  },
+  {
+    "revision": "106251ffb94e0d48d9f0",
+    "url": "/Calcculator/static/js/runtime-main.1a80bc1a.js"
+  }
+]);
